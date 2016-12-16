c0f1df94caa096a4012ffb36fbb44be0b759fe39
Parth Dabhi (iOS & Android developer) <iparth@iMac.local>
2016-12-16 19:38:46 +0530

    - added expandable cell to main category screen

--- Postnatal Depression/MainCategoryVC.swift
+++ Postnatal Depression/MainCategoryVC.swift
@@ -12,7 +12,9 @@ class MainCategoryVC: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tblCategory: UITableView!
     
-    var items: [String] = ["cat1", "cat2", "cat3", "cat4", "cat5", "cat6"]
+    var categories:Array<String> = ["category 1","category 2","category 3","category 4"]
+    var items:Dictionary<String,AnyObject> = ["category 1":["cat 11", "cat 12","cat 13", "cat 14"],"category 2":["cat 21", "cat 22","cat 23"],"category 3":["cat 31", "cat 32","cat 33", "cat 34"],"category 4":["cat 41", "cat 42"]]
+    var selectedSections:Array<Int> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,26 +44,91 @@ class MainCategoryVC: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.navigationController!.popViewControllerAnimated(true)
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return categories.count
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return items.count
-        return 15
+        //return 15
+        
+        //selectedSections?.contains(section)
+        if selectedSections.contains(section) {
+            //Unselect section
+            let category = categories[section]
+            let subCats = items[category]
+            return 1 + ((subCats?.count) ?? 0)
+        } else {
+            //Expand submenu
+            return 1
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell:CategoryTableViewCell = self.tblCategory.dequeueReusableCellWithIdentifier("CategoryTableViewCell") as! CategoryTableViewCell
-        
-        cell.lblCategoryTitle?.text = "Category \(indexPath.row)"
-        
-        cell.imgStatus.backgroundColor = UIColor.lightTextColor()
-        cell.imgStatus.layer.cornerRadius = (cell.imgStatus.frame.width/2)
-        cell.imgStatus.layer.masksToBounds = true
-        
-        return cell
+        if indexPath.row == 0 {
+            //Main Category
+            let cell:CategoryTableViewCell = self.tblCategory.dequeueReusableCellWithIdentifier("CategoryTableViewCell") as! CategoryTableViewCell
+            
+            cell.lblCategoryTitle?.text = "Category \(indexPath.section+1) \(indexPath.row+1)"
+            
+            cell.imgStatus.backgroundColor = UIColor.lightTextColor()
+            cell.imgStatus.layer.cornerRadius = (cell.imgStatus.frame.width/2)
+            cell.imgStatus.layer.masksToBounds = true
+            
+            cell.imgStatus.hidden = false
+            cell.imgCategory.hidden = false
+            
+            return cell
+        } else {
+            //Sub menu
+            let cell:CategoryTableViewCell = self.tblCategory.dequeueReusableCellWithIdentifier("CategoryTableViewCell") as! CategoryTableViewCell
+            
+            let array = items[categories[indexPath.section]] as? Array<String> ?? []
+            cell.lblCategoryTitle?.text = "- \(array[indexPath.row-1])"
+            
+            cell.imgStatus.hidden = true
+            cell.imgCategory.hidden = true
+            cell.imgCategory.backgroundColor = UIColor.lightTextColor()
+            cell.imgCategory.layer.cornerRadius = (cell.imgStatus.frame.width/2)
+            cell.imgCategory.layer.masksToBounds = true
+            
+            return cell
+        }
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        guard  else {
+//            //selection of first cell to expand
+//            
+//            return
+//        }
         
+        if selectedSections.contains(indexPath.section) {
+            if indexPath.row == 0 {
+                //Unselect section
+                selectedSections.removeAtIndex(selectedSections.indexOf(indexPath.section) ?? 0)
+                //self.tblCategory.reloadData()
+                self.tblCategory.reloadSections(NSIndexSet(index: indexPath.section), withRowAnimation: UITableViewRowAnimation.Fade)
+            } else {
+                //Value Selected
+                
+            }
+        } else {
+            //Expand submenu
+            
+            if selectedSections.count > 0 {
+                //self.tblCategory.reloadSections(NSIndexSet(index: selectedSections!), withRowAnimation: UITableViewRowAnimation.None)
+                selectedSections.append(indexPath.section)
+                //self.tblCategory.reloadData()
+                self.tblCategory.reloadSections(NSIndexSet(index: indexPath.section), withRowAnimation: UITableViewRowAnimation.Fade)
+                return
+            }
+            
+            selectedSections.append(indexPath.section)
+            //self.tblCategory.reloadData()
+            self.tblCategory.reloadSections(NSIndexSet(index: indexPath.section), withRowAnimation: UITableViewRowAnimation.Fade)
+        }
     }
     
 }
